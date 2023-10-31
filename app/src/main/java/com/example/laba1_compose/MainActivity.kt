106962a5a7ffe23e3a4ed2fca54e56b847fe5924
Mike Tsvirkunov <mike.tsvirkunov@gmail.com>
2023-10-31 22:52:46 +0600

    First

---
+++ app/src/main/java/com/example/laba1_compose/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.laba1_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.laba1_compose.ui.theme.LABA1_composeTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LABA1_composeTheme {
+    }
+}
